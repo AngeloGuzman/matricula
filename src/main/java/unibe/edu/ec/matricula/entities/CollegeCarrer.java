f8c9d779b369d2c63c7f3c7bbdb617deef2a46ad
Angelo <sofux1996@gmail.com>
2019-11-26 11:40:53 -0500

    Initial commit

---
+++ src/main/java/unibe/edu/ec/matricula/entities/CollegeCarrer.java
@@ -0,0 +1,21 @@
+package unibe.edu.ec.matricula.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class CollegeCarrer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int semester;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Teacher decade;
+
+    @ManyToMany(mappedBy = "collegeCarrers" )
+    private List<Student> students;
+
+
+}
---
+++ src/main/java/unibe/edu/ec/matricula/entities/Student.java
@@ -0,0 +1,45 @@
+package unibe.edu.ec.matricula.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Student extends Person {
+
+
+    private boolean status;
+    @JoinTable(
+            name = "student_collegeCareer",
+            joinColumns = @JoinColumn(name = "student",nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "collegeCarrer", nullable = false)
+    )
+
+
+    @ManyToMany(cascade = CascadeType.ALL)
+
+    private List<CollegeCarrer> collegeCarrers;
+    public Student() {
+
+    }
+
+    public Student(String id, String name, String address, String phoneNumber, String email, Date birthDate, boolean status) {
+        super(id, name, address, phoneNumber, email, birthDate);
+        this.status = status;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "status=" + status +
+                '}';
+    }
+}
